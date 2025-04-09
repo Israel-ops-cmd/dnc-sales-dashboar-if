cc1b61e9d7e7d5168b036de7ab6aced9f0cad145
Israel-ops-cmd <israellipe2020@gmail.com>
2025-04-09 14:38:30 -0300

    Feat: Form component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
-import { BannerImage } from "@/components";
+import { BannerImage, FormComponent, Logo,  StyledH1, StyledP } from "@/components";
+import { pxToRem } from "@/utils";
 
 
 
@@ -8,8 +9,26 @@ function Login() {
     return (
         <>
             <Box sx={{ display: "flex", height: "100vh", width: "100vw" }}>
-                <Box sx={{ width: "50vw", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    <h1>LOGIN</h1>
+                <Box sx={{ width: "50vw", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                    <Box sx={{ marginBottom: pxToRem(24) }}>
+                        <Logo height={41} width={100}/>
+                    </Box>
+                    <Box sx={{ marginBottom: pxToRem(24) }}>
+                        <StyledH1>Bem-vindo</StyledH1>
+                        <StyledP>Digite sua senha e email para logar</StyledP>
+                    </Box>
+                    <FormComponent inputs={[
+                        {type: 'email', placeholder: 'Email', disabled: true},
+                        {type: 'password', placeholder: 'Senha'}
+                    ]} 
+                    buttons = {[
+                        { className: 'primary', type: 'submit', children: 'Login', disabled: true}
+                    ]}
+                    message = {{
+                        msg: 'Sucesso!!!',
+                        type: 'success'
+                    }}
+                    />
                 </Box>
                 <Box sx={{ width: "50vw", display: { xs: "none", sm: "block" } }}>
                     <BannerImage />
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
-import { BannerImage } from "@/components";
+import { BannerImage, FormComponent, Logo, StyledH1, StyledP, StyledUl } from "@/components";
+import { pxToRem } from "@/utils";
 
 
 
@@ -7,8 +8,32 @@ function Registration() {
     return (
         <>
             <Box sx={{ display: "flex", height: "100vh", width: "100vw" }}>
-            <Box sx={{ width: "50vw", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <h1>CADASTRO</h1>
+            <Box sx={{ width: "50vw", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                <Box sx={{ marginBottom: pxToRem(24) }}>
+                    <Logo height={41} width={100}/>
+                </Box>
+                <Box sx={{ marginBottom: pxToRem(24) }}>
+                    <StyledH1>Faça seu cadastro</StyledH1>
+                    <StyledP>Primeiro, diga-nos quem você é.</StyledP>
+                    <StyledUl>
+                        <li>Entre 8 e 16 caracteres;</li>
+                        <li>Pelo menos uma letra maiúscula;</li>
+                        <li>Pelo menos um caractere especial;</li>
+                        <li>Pelo menos um número</li>
+                    </StyledUl>
+                </Box>
+                <FormComponent inputs={[
+                        {type: 'email', placeholder: 'Email'},
+                        {type: 'password', placeholder: 'Senha'}
+                    ]} 
+                    buttons = {[
+                        { className: 'primary', type: 'submit', children: 'Login'}
+                    ]}
+                    message = {{
+                        msg: 'ERRO!!!',
+                        type: 'error'
+                    }}
+                    />
             </Box>
             <Box sx={{ width: "50vw", display: { xs: "none", sm: "block" } }}>
                 <BannerImage />
