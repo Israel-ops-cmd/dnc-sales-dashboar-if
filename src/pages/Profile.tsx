9740db00c2b8de5a28030ac39bdb48433a3ab931
Israel-ops-cmd <israellipe2020@gmail.com>
2025-04-28 17:44:13 -0300

    Feat: Profile Request

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,15 +1,105 @@
-import { useContext } from "react";
-import { AppThemeContext } from "@/contexts/AppThemeContext";
+import { ChangeEvent, useContext, useEffect, useState } from "react"
+import { AppThemeContext } from "@/contexts/AppThemeContext"
+import Cookies from "js-cookie"
 
 // COMPONENTS
-import { CardComponent, Header, StyledH2, StyledButton } from "@/components";
-import { Container, Grid} from "@mui/material";
+import { CardComponent, FormComponent, Header, StyledH2, StyledButton } from "@/components"
+import { Container, Grid} from "@mui/material"
+
+// HOOK
+import { useFormValidation, useGet, usePut, useDelete } from "@/hooks"
 
 // SERVICES
-import { logout } from "@/services";
+import { logout } from "@/services"
+
+// TYPES
+import { InputProps, ProfileData, ProfileEditableData, MessageProps } from "@/types"
+import { clear } from "node:console"
+
+
 
 function Profile() {
     const themeContext = useContext(AppThemeContext);
+
+    // HOOKS
+    const [updateMessage, setUpdateMessage] = useState<MessageProps>({
+        type: 'success',
+        msg: ''
+    })
+    const clearMessage = () => {
+        setTimeout(() => {
+            setUpdateMessage({
+                type: 'success',
+                msg: ''
+            })
+        }, 3000)
+    }
+    const {
+            data: profileData,
+            loading: profileLoading,
+            error: profileError,
+    } = useGet<ProfileData>('profile')
+
+    const {
+        data: profileUpdateData,
+        putData: profilePutData,
+        loading: profileUpdateLoading,
+        error: profileUpdateError,
+    } = usePut<ProfileEditableData>('profile/update')
+
+    const { deleteData: profileDeleteData, loading: profileDeleteLoading } = useDelete('profile/update')
+
+    useEffect(() => {
+        if(profileData) {
+            handleChange(0, profileData.name)
+            handleChange(1, profileData.email)
+            handleChange(2, profileData.phone)
+        }
+    }, [profileData])
+    
+    // FORM
+    const inputs: InputProps[] = [
+        { name: 'name', type: 'text', placeholder: 'Nome', required: true },
+        { name: 'email', type: 'email', placeholder: 'Email', disabled: true },
+        { name: 'phone', type: 'cel', placeholder: 'Telefone', required: true },
+        ]
+    const { formValues, formValid, handleChange } = useFormValidation(inputs)
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        await profilePutData(
+            {
+                name: String(formValues[0]),
+                phone: String(formValues[2])
+            }
+        )
+    }
+    const handleDelete = async () => {
+        if(confirm('Tem certeza que deseja excluir sua conta? Se sim, certifique-se de deletar os seus leads antes.')){
+            try {
+                await profileDeleteData()
+                alert('Perfil deletado com sucesso!')
+                Cookies.remove('Authorization')
+                window.location.href = '/'
+            } catch (e) {
+                alert('Não foi possível realizar a operação. Entre em contato com o nosso suporte.')
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (profileUpdateData !== null) {
+            setUpdateMessage({
+                msg: 'Perfil Atualizado com  sucesso',
+                type: 'success'
+            })
+        } else if (profileUpdateError) {
+            setUpdateMessage({
+                msg: 'Não foi possível realizar a operação. Entre em contato com o nosso suporte.',
+                type: 'success'
+            }) 
+        }
+        clearMessage()
+    }, [profileUpdateData, profileUpdateError])
 
     return (
         <>
@@ -17,7 +107,46 @@ function Profile() {
             <Container className="mb-2" maxWidth="lg">
                 <Grid container spacing={4}>
                     <Grid component="div" sx={{ width: { xs: '100%', sm: 'calc(50% - 16px)' } }}>
-                        <CardComponent>Seus dados...</CardComponent>
+                        {!profileError && (
+                            <CardComponent className={ profileLoading ? 'skeleton-loading skeleton-loading-mh-2' : ''}>
+                                {
+                                    !profileLoading && profileData && (
+                                        <>
+                                            <StyledH2 className="mb-1">Seus Dados</StyledH2>
+                                            <FormComponent 
+                                                inputs={inputs.map((input, index) => ({
+                                                ...input,
+                                                type: input.type,
+                                                placeholder: input.placeholder,
+                                                value: formValues[index] || '',
+                                                onChange: (e: ChangeEvent<HTMLInputElement>) => handleChange(index, (e.target as HTMLInputElement).value)
+                                                }))}
+                                                buttons = {
+                                                    [
+                                                        { 
+                                                            className: 'primary', 
+                                                            disabled: !formValid || profileUpdateLoading,  
+                                                            type: 'submit', 
+                                                            onClick: handleSubmit, 
+                                                            children: profileUpdateLoading ? 'Aguarde...' : 'Atualizar meu perfil'
+                                                        },
+                                                        { 
+                                                            className: 'alert',
+                                                            disabled: profileDeleteLoading,   
+                                                            type: 'button', 
+                                                            onClick: handleDelete, 
+                                                            children: profileDeleteLoading? 'Aguarde...' : 'Excluir minha conta'
+                                                        }
+                                                    ]
+                                                }
+                                                message={updateMessage}
+                                            />
+                                        </>
+                                    )
+                                }
+                            </CardComponent>
+                        )
+                        }
                     </Grid>
                     <Grid component="div" sx={{ width: { xs: '100%', sm: 'calc(50% - 16px)' } }}>
                         <CardComponent>
